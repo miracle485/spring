3991c640f398472bed1ad5f5c57c04ec460d5eb5
wangyilin29 <wangyilin29@meituan.com>
2023-11-06 20:46:01 +0800

    [feature] 完善es客户端创建销毁逻辑

--- MysqlBinlog/src/main/java/com/example/mysqlbinlog/manager/EsClientManager.java
+++ MysqlBinlog/src/main/java/com/example/mysqlbinlog/manager/EsClientManager.java
@@ -1,14 +1,67 @@
 package com.example.mysqlbinlog.manager;
 
 import com.google.common.collect.Maps;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Service
 public class EsClientManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EsClientManager.class);
     private final Map<String, RestHighLevelClient> restHighLevelClientMap = Maps.newConcurrentMap();
 
+    public RestHighLevelClient getClientByUrl(String host, int port) {
+        if (StringUtils.isEmpty(host)) {
+            return null;
+        }
+        RestHighLevelClient restHighLevelClient = MapUtils.getObject(restHighLevelClientMap, buildClientKey(host, port));
+        if (restHighLevelClient == null) {
+            restHighLevelClient = createElasticSearchClient(host, port);
+        }
+
+        return restHighLevelClient;
+    }
+
+    private synchronized RestHighLevelClient createElasticSearchClient(String host, int port) {
+        String key = buildClientKey(host, port);
+        if (restHighLevelClientMap.containsKey(key)) {
+            return restHighLevelClientMap.get(key);
+        }
+        HttpHost http = new HttpHost(host, port, "http");
+        return new RestHighLevelClient(RestClient.builder(http));
+    }
+
+    private String buildClientKey(String host, int port) {
+        return host + ":" + port;
+    }
+
+    public void shutdownAll() {
+        restHighLevelClientMap.forEach(this::shutdownWithKey);
+    }
+
+    public void shutdownByUrl(String host, int port) {
+        String key = buildClientKey(host, port);
+        shutdownWithKey(key, restHighLevelClientMap.get(key));
+    }
+
+    private void shutdownWithKey(String key, RestHighLevelClient client) {
+        if (client == null) {
+            return;
+        }
+        try {
+            client.close();
+        } catch (IOException e) {
+            LOGGER.error("error when close es client, config is {}", key);
+        }
+    }
 
 }
